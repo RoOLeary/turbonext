439fb22efa517b6fac520b5b1231d44b0a3b2b50
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2023-06-29 13:49:32 +0200

    scratch branch test with nesting analytics

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from 'react';
+import { Analytics } from "../components/Analytics";
 import "../styles/globals.css";
 // include styles from the ui package
 import "ui/styles.css";
+
 
 export default function RootLayout({
   children,
@@ -9,7 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-zinc-900">
-      <body>{children}</body>
+      <body>
+        <>
+          <Analytics />
+          {children}
+        </>
+      </body>
     </html>
   );
 }
